3355d1aae326cf28036c53f6416c2e8d55338703
Maciej Majek <maciej.majek@robotec.ai>
2024-06-12 15:45:58 +0200

    feat: update husarion poc example

--- examples/husarion_poc_example.py
+++ examples/husarion_poc_example.py
@@ -12,7 +12,6 @@ from langchain_core.messages import (
 from langchain_openai import ChatOpenAI
 from rich import print
 
-from rai.langchain_extension.tooling import RaiToolMessage
 from rai.tools.hmi_tools import PlayVoiceMessageTool, WaitForSecondsTool
 from rai.tools.ros_cli_tools_simple import (
     Ros2InterfaceTool,
@@ -20,46 +19,13 @@ from rai.tools.ros_cli_tools_simple import (
     Ros2TopicTool,
     SetGoalPoseTool,
 )
-from rai.tools.ros_tools import GetCameraImageTool, GetOccupancyGridTool
-
-
-def run_tool_call(tool_call: ToolCall, tools: Sequence[BaseTool]) -> Any:
-    selected_tool = {k.name: k for k in tools}[tool_call["name"]]
-    try:
-        args = selected_tool.args_schema(**tool_call["args"])  # type: ignore
-    except Exception as e:
-        return f"Error in preparing arguments for {selected_tool.name}: {e}"
-
-    print(f"Running tool: {selected_tool.name} with args: {args.dict()}")
-
-    try:
-        tool_output = selected_tool.run(args.dict())
-    except Exception as e:
-        return f"Error running tool {selected_tool.name}: {e}"
-
-    return tool_output
-
-
-def run_requested_tools(
-    ai_msg: AIMessage, tools: Sequence[BaseTool], messages: List[AnyMessage]
-):
-    for tool_call in ai_msg.tool_calls:
-        tool_output = run_tool_call(tool_call, tools)
-        if isinstance(tool_output, dict):
-            tool_message = RaiToolMessage(
-                content=tool_output.get("content", ""),  # type: ignore
-                images=tool_output.get("images", []),  # type: ignore
-                tool_call_id=tool_call["id"],  # type: ignore
-            ).to_openai()
-        else:
-            tool_message = [
-                ToolMessage(
-                    content=str(tool_output), tool_call_id=tool_call["id"]  # type: ignore
-                )
-            ]
-        messages.extend(tool_message)
-
-    return messages
+from rai.tools.ros_tools import (
+    GetCameraImageTool,
+    GetCurrentPositionTool,
+    GetOccupancyGridTool,
+    SetWaypointTool,
+)
+from rai.tools.utils import run_requested_tools
 
 
 def main():
@@ -72,6 +38,8 @@ def main():
         Ros2ServiceTool(),
         Ros2InterfaceTool(),
         SetGoalPoseTool(),
+        SetWaypointTool(),
+        GetCurrentPositionTool(),
     ]
 
     messages: List[AnyMessage] = [
@@ -82,7 +50,9 @@ def main():
             "You are always required to send a voice message to the user about your decisions. This is crucial."
             "The voice message should contain a very short information about what is going on and what is the next step. "
         ),
-        HumanMessage(content="Visit every location on the map."),
+        HumanMessage(
+            content="The robot is moving. Use vision to understand the surroundings, and add waypoints based on observations. camera is accesible at topic /camera/camera/color/image_raw ."
+        ),
     ]
 
     llm = ChatOpenAI(model="gpt-4o")
