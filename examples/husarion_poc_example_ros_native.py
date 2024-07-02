b4f714bec5284e11021f290adc16dc8319bab49c
Bartek Boczek <22739059+boczekbartek@users.noreply.github.com>
2024-07-02 18:13:10 +0200

    feat: add simple rclpy based function calling (#11)
    * feat(tools): rclpy based ros2 publisher and single msg reader
    
    * refactor
    
    * fix: remove misleading docstrings
    
    * add typehints
    
    * fix: rclpy ros tools docs
    
    * don't add frame_id to msg header by default
    
    * Update src/rai/tools/ros/utils.py
    
    Co-authored-by: Maciej Majek <46171033+maciejmajek@users.noreply.github.com>
    
    * fix: ros2 pub tool
    
    * refactor: rename `rclpy` scripts to `native`
    
    - discussion: https://github.com/RobotecAI/rai-private/pull/11#issuecomment-2194467117
    
    * ros native tools refactor to use global node
    
    * feat(`tool_runner`): allow empty args_schema + logging
    
    chore: pre-commit
    
    * revert('ScenarioRunner`): convert to node
    
    * refactor: change logging to logger
    
    * logger.error -> logger.warning for tool call error
    
    * fix: husarion_poc_example_ros_native
    
    * fix logger
    
    * Update src/rai/scenario_engine/tool_runner.py
    
    Co-authored-by: Maciej Majek <46171033+maciejmajek@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Maciej Majek <46171033+maciejmajek@users.noreply.github.com>

---
+++ examples/husarion_poc_example_ros_native.py
@@ -0,0 +1,62 @@
+import logging
+import os
+from typing import List
+
+import rclpy
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+from rclpy.node import Node
+
+from rai.scenario_engine.messages import AgentLoop
+from rai.scenario_engine.scenario_engine import ScenarioPartType, ScenarioRunner
+from rai.tools.ros.cat_demo_tools import FinishTool
+from rai.tools.ros.native import (
+    Ros2GetOneMsgFromTopicTool,
+    Ros2GetTopicsNamesAndTypesTool,
+    Ros2PubMessageTool,
+)
+
+
+def main():
+    scenario: List[ScenarioPartType] = [
+        SystemMessage(
+            content="You are an autonomous agent. Your main goal is to fulfill the user's requests. "
+            "Do not make assumptions about the environment you are currently in. "
+            "Use the tooling provided to gather information about the environment."
+        ),
+        HumanMessage(content="The robot is moving. Send robot to the random location"),
+        AgentLoop(stop_action=FinishTool().__class__.__name__, stop_iters=50),
+    ]
+
+    log_usage = all((os.getenv("LANGFUSE_PK"), os.getenv("LANGFUSE_SK")))
+    llm = ChatOpenAI(model="gpt-4o")
+
+    rclpy.init()
+
+    rai_node = Node("rai")  # type: ignore
+
+    tools = [
+        Ros2GetTopicsNamesAndTypesTool(),
+        Ros2GetOneMsgFromTopicTool(node=rai_node),
+        Ros2PubMessageTool(node=rai_node),
+        FinishTool(),
+    ]
+
+    runner = ScenarioRunner(
+        scenario,
+        llm,
+        tools=tools,
+        llm_type="openai",
+        scenario_name="Husarion example",
+        log_usage=log_usage,
+        logging_level=logging.INFO,
+    )
+
+    runner.run()
+    rai_node.destroy_node()
+
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
