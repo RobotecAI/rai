00b6315a358b6686594bf4d807990fdb5868a5fc
Magdalena Kotynia <magdalena.kotynia@robotec.ai>
2024-07-02 14:22:16 +0200

    feat: added publishing string transcription messages by a topic

--- src/asr/asr/asr_node.py
+++ src/asr/asr/asr_node.py
@@ -9,6 +9,7 @@ import torch
 import whisper
 from rclpy.node import Node
 from scipy.io import wavfile
+from std_msgs.msg import String
 
 SAMPLING_RATE = 16000
 
@@ -22,14 +23,17 @@ model, utils = torch.hub.load(
 
 class ASRNode(Node):
     def __init__(self):
-        super().__init__("sound_capture_node")
+        super().__init__("automatic_speech_recognition")
         self.sample_rate = SAMPLING_RATE
         self.is_recording = False
         self.audio_buffer = []
         self.thread = None
         self.vad_iterator = VADIterator(model, sampling_rate=self.sample_rate)
         self.silence_start_time = None
-        self.grace_period = timedelta(seconds=2)  # 1 second grace period for silence
+        self.grace_period = timedelta(seconds=2)
+        self.transcription_publisher = self.create_publisher(
+            String, "transcription", 10
+        )
         self.get_logger().info(
             "Voice Activity Detection enabled. Waiting for speech..."
         )
@@ -45,12 +49,10 @@ class ASRNode(Node):
         stream.start()
 
         while True:
-            # print("Silence start time: ", self.silence_start_time)
             audio_data, _ = stream.read(window_size_samples)
             audio_data = audio_data.flatten()
 
             speech_dict = self.vad_iterator(audio_data, return_seconds=True)
-            # print("Speech dict: ", speech_dict)
             if speech_dict:
                 if not self.is_recording:
                     self.start_recording()
@@ -60,10 +62,6 @@ class ASRNode(Node):
                     self.silence_start_time = datetime.now()
             elif self.is_recording:
                 self.audio_buffer.append(audio_data)
-                # if speech_dict is not None:
-                #     if 'end' in speech_dict.keys():
-                #         print("END Speech dict: ", speech_dict)
-                #         self.silence_start_time = datetime.now()
                 if self.silence_start_time is not None:
                     if datetime.now() - self.silence_start_time > self.grace_period:
                         self.stop_recording_and_transcribe()
@@ -83,15 +81,21 @@ class ASRNode(Node):
 
     def transcribe_audio(self):
         combined_audio = np.concatenate(self.audio_buffer)
-        # print("Combined audio shape:", combined_audio.shape)
         with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_wav_file:
             wavfile.write(temp_wav_file.name, self.sample_rate, combined_audio)
             self.get_logger().info(f"Saved audio to {temp_wav_file.name}")
 
             model = whisper.load_model("base")
             response = model.transcribe(temp_wav_file.name, language="en")
-            self.get_logger().info(f'Transcription: {response["text"]}')
+            transcription = response["text"]
+            self.get_logger().info(f"Transcription: {transcription}")
+            self.publish_transcription(transcription)
         self.audio_buffer = []
+
+    def publish_transcription(self, transcription):
+        msg = String()
+        msg.data = transcription
+        self.transcription_publisher.publish(msg)
 
 
 def main(args=None):
