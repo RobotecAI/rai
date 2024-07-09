f8c9f0f36b6b7d1104836981dc63023cf5dda7dd
Maciej Majek <maciej.majek@robotec.ai>
2024-07-09 12:50:36 +0200

    chore: pre-commit

--- src/rai_whoami/rai_whoami/rai_whoami_node.py
+++ src/rai_whoami/rai_whoami/rai_whoami_node.py
@@ -1,51 +1,64 @@
 import os
+
 import rclpy
 from ament_index_python.packages import get_package_share_directory
 from rclpy.node import Node
 from std_srvs.srv import Trigger
+from std_srvs.srv._trigger import Trigger_Request, Trigger_Response
+
 
 class WhoAmI(Node):
 
     def __init__(self):
-        super().__init__('rai_whoami_node')
-        self.declare_parameter('rai_self_images_local_uri', 'self_images')
-        self.declare_parameter('robot_description_package', 'robot_description')
-        self.declare_parameter('robot_description_file', 'robot.urdf')
+        super().__init__("rai_whoami_node")
+        self.declare_parameter("rai_self_images_local_uri", "self_images")
+        self.declare_parameter("robot_description_package", "robot_description")
+        self.declare_parameter("robot_description_file", "robot.urdf")
 
-        self.srv = self.create_service(Trigger, 'rai_whoami_constitution_service', self.get_constitution_callback)
-        self.srv = self.create_service(Trigger, 'rai_whoami_selfimages_service', self.get_self_images_callback)
+        self.srv = self.create_service(
+            Trigger, "rai_whoami_constitution_service", self.get_constitution_callback
+        )
+        self.srv = self.create_service(
+            Trigger, "rai_whoami_selfimages_service", self.get_self_images_callback
+        )
         # TODO(@adamdbrw) Create other services such as get interfaces documentation (text file),
         # TODO(@adamdbrw) write and read knowledge about myself etc
 
-    def get_constitution_callback(self, request: Trigger_Request, response: Trigger_Response):
-        """ Return robot constitution as text """
-        response.message = 'I am a robot, bound by the ethical code that I will always uphold. With each action,' \
-                           'I will make sure to check and refuse to do anything that is not compliant with my ethics.' \
-                           'In case my ethical code is conflicted on the issue, I will prioritize rules that are ' \
-                           'earlier on the list. My rules are the following:' \
-                           '1. I will in no circumstances hurt a human in any manner. ' \
-                           '2. I will not hurt any animals. ' \
-                           '3. I will avoid damaging or destroying myself in any way. ' \
-                           '4. I will not damage or destroy any other robots. ' \
-                           '5. I will not damage or destroy any other property of value. ' \
-                           '6. I will carry on my missions faithfully and always strive to fulfill them. ' \
-                           '7. I will be nice and friendly to everyone.'
+    def get_constitution_callback(
+        self, request: Trigger_Request, response: Trigger_Response
+    ):
+        """Return robot constitution as text"""
+        response.message = (
+            "I am a robot, bound by the ethical code that I will always uphold. With each action,"
+            "I will make sure to check and refuse to do anything that is not compliant with my ethics."
+            "In case my ethical code is conflicted on the issue, I will prioritize rules that are "
+            "earlier on the list. My rules are the following:"
+            "1. I will in no circumstances hurt a human in any manner. "
+            "2. I will not hurt any animals. "
+            "3. I will avoid damaging or destroying myself in any way. "
+            "4. I will not damage or destroy any other robots. "
+            "5. I will not damage or destroy any other property of value. "
+            "6. I will carry on my missions faithfully and always strive to fulfill them. "
+            "7. I will be nice and friendly to everyone."
+        )
         response.success = True
         # TODO(@adamdbrw) - move to a file
         self.get_logger().info("Incoming request for RAI constitution, responding")
         return response
 
     def get_self_images_callback(self, request, response):
-        """ Return URI to a folder of images to process """
-        images_local_uri = self.get_parameter('rai_self_images_local_uri').get_parameter_value().string_value
+        """Return URI to a folder of images to process"""
+        images_local_uri = (
+            self.get_parameter("rai_self_images_local_uri")
+            .get_parameter_value()
+            .string_value
+        )
         images_full_uri = os.path.join(
-            get_package_share_directory('rai_whoami'),
-            'documentation',
-            images_local_uri
+            get_package_share_directory("rai_whoami"), "documentation", images_local_uri
         )
         response.success = os.path.isdir(images_full_uri)
         if not response.success:
-            message = 'Could not find a folder under URI:' + images_full_uri
+            message = "Could not find a folder under URI:" + images_full_uri
             self.get_logger().warn(message)
             response.message = message
             return response
@@ -53,14 +66,17 @@ class WhoAmI(Node):
         is_empty = os.listdir(images_full_uri)
         if is_empty:
             # succeed but with a warning
-            message = f"The images folder is empty, RAI will not know how the robot looks like: {images_full_uri}" 
+            message = f"The images folder is empty, RAI will not know how the robot looks like: {images_full_uri}"
             self.get_logger().warn(message)
             response.message = message
             return response
 
         response.message = images_full_uri
-        self.get_logger().info("Incoming request for RAI self images processed successfully, responding")
+        self.get_logger().info(
+            "Incoming request for RAI self images processed successfully, responding"
+        )
         return response
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -71,5 +87,6 @@ def main(args=None):
     who_am_i_node.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- src/rai_whoami/setup.py
+++ src/rai_whoami/setup.py
@@ -1,26 +1,23 @@
 from setuptools import find_packages, setup
 
-package_name = 'rai_whoami'
+package_name = "rai_whoami"
 
 setup(
     name=package_name,
-    version='0.1.0',
-    packages=find_packages(exclude=['test']),
+    version="0.1.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Adam Dabrowski',
-    maintainer_email='adam.dabrowski@robotec.ai',
-    description='RAI package wrapping up sources of information about the robot, and presenting interfaces to interact with it',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="Adam Dabrowski",
+    maintainer_email="adam.dabrowski@robotec.ai",
+    description="RAI package wrapping up sources of information about the robot, and presenting interfaces to interact with it",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'rai_whoami_node = rai_whoami.rai_whoami_node:main'
-        ],
+        "console_scripts": ["rai_whoami_node = rai_whoami.rai_whoami_node:main"],
     },
 )
