e8cd7340f22fbd09666ec07630512a039b0d78f9
Maciej Majek <maciej.majek@robotec.ai>
2024-07-18 14:15:58 +0200

    feat: parametrize hri launch file

--- src/rai_bringup/launch/hri.launch.py
+++ src/rai_bringup/launch/hri.launch.py
@@ -1,11 +1,38 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
+def include_tts_launch(context, *args, **kwargs):
+    tts_vendor = LaunchConfiguration("tts_vendor").perform(context)
+    if tts_vendor == "opentts":
+        launch_file = "opentts.launch.py"
+    else:
+        launch_file = "elevenlabs.launch.py"
+
+    return [
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [FindPackageShare("rai_tts"), f"/launch/{launch_file}"]
+            )
+        )
+    ]
+
+
 def generate_launch_description():
+    tts_vendor_arg = DeclareLaunchArgument(
+        "tts_vendor",
+        default_value="elevenlabs",
+        description="TTS vendor to use (opentts or elevenlabs)",
+    )
+
     asr_node = Node(
         package="rai_asr", executable="asr_node", name="asr_node", output="screen"
     )
@@ -14,10 +41,6 @@ def generate_launch_description():
         package="rai_hmi", executable="hmi_node", name="hmi_node", output="screen"
     )
 
-    tts_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [FindPackageShare("rai_tts"), "/launch/elevenlabs.launch.py"]
-        )
-    )
+    tts_launch_inclusion = OpaqueFunction(function=include_tts_launch)
 
-    return LaunchDescription([asr_node, hmi_node, tts_launch])
+    return LaunchDescription([tts_vendor_arg, asr_node, hmi_node, tts_launch_inclusion])
