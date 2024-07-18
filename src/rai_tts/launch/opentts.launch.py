93e2bb475fd83476f7af051a1f8bac902c0ed302
Maciej Majek <46171033+maciejmajek@users.noreply.github.com>
2024-07-18 13:40:07 +0200

    feat: implement rai-tts node (#83)
    * feat: implement rai-tts node
    
    * refactor: allow empty elevenlabs api key
    
    * chore: remove poetry lock

---
+++ src/rai_tts/launch/opentts.launch.py
@@ -0,0 +1,29 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory("rai_tts"), "config", "opentts.yaml"
+    )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "config_file",
+                default_value=config,
+                description="Path to the config file",
+            ),
+            Node(
+                package="rai_tts",
+                executable="tts_node",
+                name="tts_node",
+                parameters=[LaunchConfiguration("config_file")],
+            ),
+        ]
+    )
