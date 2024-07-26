40903284d310525d79817ed5ceb60a3e5e717e2e
Wiktoria Siekierska <wiktoria.siekierska@robotec.ai>
2024-07-26 16:42:34 +0200

    husarion_whoami package
    Signed-off-by: Wiktoria Siekierska <wiktoria.siekierska@robotec.ai>

---
+++ src/examples/husarion_whoami/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+package_name = "husarion_whoami"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="wsiekierska",
+    maintainer_email="wiktoria.siekierska@robotec.ai",
+    description="TODO: Package description",
+    license="Apache-2.0",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [],
+    },
+)
--- src/rai/cli/rai_cli.py
+++ src/rai/cli/rai_cli.py
@@ -1,4 +1,5 @@
 import argparse
+import pathlib
 import subprocess
 from pathlib import Path
 
@@ -6,8 +7,16 @@ from pathlib import Path
 def create_robot_package():
     parser = argparse.ArgumentParser(description="Creation of a robot package.")
     parser.add_argument("--name", type=str, required=True, help="Package's name")
+    parser.add_argument(
+        "--destination-directory",
+        type=str,
+        required=False,
+        default="src/",
+        help="Package's destination",
+    )
     args = parser.parse_args()
     package_name = args.name
+    package_destination = args.destination_directory
 
     subprocess.run(
         [
@@ -18,18 +27,25 @@ def create_robot_package():
             "ament_python",
             f"{package_name}_whoami",
             "--destination-directory",
-            "src/",
+            f"{package_destination}/",
             "--license",
             "Apache-2.0",
         ]
     )
 
-    package_path = Path(f"src/{package_name}_whoami/description")
+    package_path = Path(f"{package_destination}/{package_name}_whoami/description")
     package_path.mkdir(parents=True, exist_ok=True)
 
     (package_path / "documentation").mkdir(exist_ok=True)
     (package_path / "images").mkdir(exist_ok=True)
     (package_path / "robot_constitution.txt").touch()
+
+    default_constitution_path = f"{pathlib.Path(__file__).parent.resolve()}/resources/default_robot_constitution.txt"
+    with open(default_constitution_path, "r") as file:
+        default_constitution = file.read()
+
+    with open(f"{package_path}/robot_constitution.txt", "w") as file:
+        file.write(default_constitution)
 
 
 if __name__ == "__main__":
