1732c7100d3fc8bd24a5ab674e9e5de0610a26c5
Bartłomiej Boczek <bartlomiej.boczek@robotec.ai>
2024-07-29 09:53:28 +0200

    revert: husarion example

--- examples/husarion_poc_example_ros_native.py
+++ examples/husarion_poc_example_ros_native.py
@@ -4,18 +4,18 @@ from typing import List
 
 import rclpy
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.tools import BaseTool
 from langchain_openai import ChatOpenAI
+from rclpy.node import Node
 
 from rai.config.models import OPENAI_MULTIMODAL
-from rai.node import RaiNode
 from rai.scenario_engine.messages import AgentLoop
 from rai.scenario_engine.scenario_engine import ScenarioPartType, ScenarioRunner
 from rai.tools.ros.cat_demo_tools import FinishTool
-from rai.tools.ros.native import (  # Ros2GetOneMsgFromTopicTool,; Ros2GetTopicsNamesAndTypesTool,; Ros2PubMessageTool,
-    Ros2ActionRunner,
-    Ros2CheckActionResults,
-    Ros2GetActionNamesAndTypesTool,
-    Ros2ShowRos2MsgInterfaceTool,
+from rai.tools.ros.native import (
+    Ros2GetOneMsgFromTopicTool,
+    Ros2GetTopicsNamesAndTypesTool,
+    Ros2PubMessageTool,
 )
 
 
@@ -26,16 +26,12 @@ def main():
 
     rclpy.init()
 
-    rai_node = RaiNode()  # type: ignore
+    rai_node = Node("rai")  # type: ignore
 
-    tools: List = [
-        # Ros2GetTopicsNamesAndTypesTool(),
-        # Ros2GetOneMsgFromTopicTool(node=rai_node),
-        # Ros2PubMessageTool(node=rai_node),
-        Ros2GetActionNamesAndTypesTool(node=rai_node),
-        Ros2ShowRos2MsgInterfaceTool(node=rai_node),
-        Ros2ActionRunner(node=rai_node),
-        Ros2CheckActionResults(node=rai_node),
+    tools: List[BaseTool] = [
+        Ros2GetTopicsNamesAndTypesTool(),
+        Ros2GetOneMsgFromTopicTool(node=rai_node),
+        Ros2PubMessageTool(node=rai_node),
         FinishTool(),
     ]
 
@@ -54,7 +50,6 @@ def main():
     runner = ScenarioRunner(
         scenario,
         llm,
-        rosnode=RaiNode(),  # type: ignore
         llm_type="openai",
         scenario_name="Husarion example",
         log_usage=log_usage,
