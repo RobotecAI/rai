da0cd37f7b7b5a02f3a889333647e6b1e039e565
Wiktoria Siekierska <115184147+wsiekierska@users.noreply.github.com>
2024-07-29 18:25:14 +0200

    feat: rai_cli and improved parameterization of rai_whoami (#93)
    * running rai_whoami_node with no parameters
    
    Signed-off-by: Wiktoria Siekierska <wiktoria.siekierska@robotec.ai>
    
    * running rai_whoami_node with no parameters
    
    Signed-off-by: Wiktoria Siekierska <wiktoria.siekierska@robotec.ai>
    
    * adding rai_cli module enabling creation of a whoami package
    
    Signed-off-by: Wiktoria Siekierska <wiktoria.siekierska@robotec.ai>
    
    * adding rai_cli module enabling creation of a whoami package
    
    Signed-off-by: Wiktoria Siekierska <wiktoria.siekierska@robotec.ai>
    
    * husarion_whoami package
    
    Signed-off-by: Wiktoria Siekierska <wiktoria.siekierska@robotec.ai>
    
    * docs: filled the information about husarion_whoami package
    
    * refactor: hardcoded path to the default robot constitution
    
    * docs: added sourcing of ros2 in env setup instruction
    
    * chore: modify data_files in automatically generated setup.py file
    
    * build: modified names of command to create rai_ws
    
    * chore: refactor and fixes of class fields
    
    * chore: replaced images of husarion_whoami with the new ones
    
    * feat: update husarion_whoami
    
    * refactor: setup.py, fix rai_whoami_node.py
    
    * chore: ignore w503 and 504 errors
    
    ---------
    
    Signed-off-by: Wiktoria Siekierska <wiktoria.siekierska@robotec.ai>
    Co-authored-by: Magdalena Kotynia <magdalena.kotynia@robotec.ai>
    Co-authored-by: Maciej Majek <maciej.majek@robotec.ai>

--- src/examples/husarion_whoami/setup.py
+++ src/examples/husarion_whoami/setup.py
@@ -1,24 +1,31 @@
+import glob
+import os
+
 from setuptools import find_packages, setup
 
-package_name = "rai_whoami"
+package_name = "husarion_whoami"
 
 setup(
     name=package_name,
-    version="0.1.0",
+    version="0.0.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
+        (os.path.join("share", package_name, os.path.dirname(p)), [p])
+        for p in glob.glob("description/**/*", recursive=True)
+        if os.path.isfile(p)
+    ]
+    + [
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        ("share/" + package_name, ["resource/default_robot_constitution.txt"]),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
-    maintainer="Adam Dabrowski",
-    maintainer_email="adam.dabrowski@robotec.ai",
-    description="RAI package wrapping up sources of information about the robot, and presenting interfaces to interact with it",
+    maintainer="wsiekierska",
+    maintainer_email="wiktoria.siekierska@robotec.ai",
+    description="TODO: Package description",
     license="Apache-2.0",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": ["rai_whoami_node = rai_whoami.rai_whoami_node:main"],
+        "console_scripts": [],
     },
 )
