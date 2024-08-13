06fb1a290b48139272ed03cd5de1161ececca373
Kajetan Rachwał <69043608+rachwalk@users.noreply.github.com>
2024-08-13 10:15:03 +0200

    feat: add grouding dino ros2 node (#81)
    * Initial commit adding interfaces and grounding dino
    
    * Update gitignore to include ros build dirs
    
    * Create README with installation instructions, reproducible install
    
    * Add launch files, finalise readme
    
    * pre-commit format
    
    * Remove unnecessary tests
    
    * Remove weights from .gitignore
    
    * Add grounding dino as poetry dependency
    
    * Update package README
    
    * Update base docs
    
    * Add missing dependancy to docs
    
    * Remove venv from .gitignore
    
    * Formatting
    
    * Add vision_msgs as dependency
    
    * Fix typos
    
    * Add automatic download of weights
    
    * Add README
    
    * Update README.md
    
    * prettier
    
    * fix build
    
    * update docs with pythonpath modification
    
    * Update src/rai_grounding_dino/README.md
    
    Co-authored-by: Bartek Boczek <22739059+boczekbartek@users.noreply.github.com>
    
    * Update src/rai_interfaces/package.xml
    
    Co-authored-by: Bartek Boczek <22739059+boczekbartek@users.noreply.github.com>
    
    * Update src/rai_interfaces/package.xml
    
    Co-authored-by: Bartek Boczek <22739059+boczekbartek@users.noreply.github.com>
    
    * Update src/rai_interfaces/package.xml
    
    Co-authored-by: Bartek Boczek <22739059+boczekbartek@users.noreply.github.com>
    
    * rm redundant file
    
    * add rosidl_generator to package xml for interfaces
    
    ---------
    
    Co-authored-by: Bartek Boczek <22739059+boczekbartek@users.noreply.github.com>

---
+++ src/rai_grounding_dino/README.md
@@ -0,0 +1,71 @@
+# RAI GroundingDINO
+
+This package provides a ROS2 Node which is an interface to the [Idea-Research GroundingDINO Model](https://github.com/IDEA-Research/GroundingDINO).
+It allows for open-set detection.
+
+## Installation
+
+In your workspace you need to have an `src` folder containing this package `rai_grounding_dino` and the `rai_interfaces` package.
+
+### Preparing the GroundingDINO
+
+Add required ROS dependencies:
+
+```
+rosdep install --from-paths src --ignore-src -r
+```
+
+### Build and run
+
+In the base directory of the `RAI` package install dependancies:
+
+```
+poetry install --with gdino
+```
+
+Source the ros installation
+
+```
+source /opt/ros/${ROS_DISTRO}/setup.bash
+```
+
+Run the build process:
+
+```
+colcon build
+```
+
+Source the local installation:
+
+```
+source ./install/setup.bash
+```
+
+Activate the poetry environment:
+
+```
+poetry shell
+export PYTHONPATH="$(dirname $(dirname $(poetry run which python)))/lib/python$(poetry run python --version | awk '{print $2}' | cut -d. -f1,2)/site-packages:$PYTHONPATH"
+```
+
+Run the ROS node using `ros2 launch`:
+
+```
+ros2 launch rai_grounding_dino gdino_launch.xml [weights_path:=PATH/TO/WEIGHTS]
+```
+
+> [!NOTE]
+> By default the weights will be downloaded to `$(ros2 pkg prefix rai_grounding_dino)/share/weights/`.
+> You can change this path if you downloaded the weights manually or moved them.
+
+### Example
+
+An example client is provided with the package as `rai_grounding_dino/talker.py`
+
+You can see it working by running:
+
+```
+ros2 launch rai_grounding_dino example_communication_launch.xml image_path:=src/rai_grounding_dino/images/sample.jpg [weights_path:=PATH/TO/WEIGHTS]
+```
+
+If everything was set up properly you should see a couple of detections with classes `dinosaur`, `dragon`, and `lizard`.
---
+++ src/rai_grounding_dino/rai_grounding_dino/grounding_dino.py
@@ -0,0 +1,119 @@
+import os
+import subprocess
+from pathlib import Path
+from typing import TypedDict
+
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+
+from rai_grounding_dino.boxer import GDBoxer
+from rai_interfaces.msg import RAIDetectionArray
+from rai_interfaces.srv import RAIGroundingDino
+
+
+class GDRequest(TypedDict):
+    classes: str
+    box_threshold: float
+    text_threshold: float
+    source_img: Image
+
+
+class GDinoService(Node):
+    def __init__(self):
+        super().__init__(node_name="grounding_dino", parameter_overrides=[])
+        self.srv = self.create_service(
+            RAIGroundingDino, "grounding_dino_classify", self.classify_callback
+        )
+        self.declare_parameter("weights_path", "")
+        try:
+            weight_path = self.get_parameter("weights_path").value
+            assert isinstance(weight_path, str)
+            if self.get_parameter("weights_path").value == "":
+                weight_path = self._init_weight_path()
+            self.boxer = GDBoxer(weight_path)
+        except Exception:
+            self.get_logger().error("Could not load model")
+            raise Exception("Could not load model")
+
+    def _init_weight_path(self) -> Path:
+        try:
+            found_path = subprocess.check_output(
+                ["ros2", "pkg", "prefix", "rai_grounding_dino"]
+            ).decode("utf-8")
+            install_path = (
+                Path(found_path.strip())
+                / "share"
+                / "weights"
+                / "groundingdino_swint_ogc.pth"
+            )
+            # make sure the file exists
+            if install_path.exists():
+                return install_path
+            else:
+                self._download_weights(install_path)
+                return install_path
+
+        except Exception:
+            self.get_logger().error("Could not find package path")
+            raise Exception("Could not find package path")
+
+    def _download_weights(self, path: Path):
+        try:
+            os.makedirs(path.parent, exist_ok=True)
+            subprocess.run(
+                [
+                    "wget",
+                    "https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth",
+                    "-O",
+                    path,
+                ]
+            )
+        except Exception:
+            self.get_logger().error("Could not download weights")
+            raise Exception("Could not download weights")
+
+    def classify_callback(self, request, response: RAIDetectionArray):
+        self.get_logger().info(
+            f"Request received: {request.classes}, {request.box_threshold}, {request.text_threshold}"
+        )
+
+        class_array = request.classes.split(",")
+        class_array = [class_name.strip() for class_name in class_array]
+        class_dict = {class_name: i for i, class_name in enumerate(class_array)}
+
+        boxes = self.boxer.get_boxes(
+            request.source_img,
+            class_array,
+            request.box_threshold,
+            request.text_threshold,
+        )
+
+        response.detections.detections = [
+            box.to_detection_msg(class_dict, self.get_clock().now().to_msg())
+            for box in boxes
+        ]
+        response.detections.header.stamp = self.get_clock().now().to_msg()
+        response.detections.detection_classes = class_array
+
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    gdino_service = GDinoService()
+
+    try:
+        rclpy.spin(gdino_service)
+    except KeyboardInterrupt:
+        gdino_service.get_logger().info("Shutting down")
+    except Exception as e:
+        gdino_service.get_logger().error(f"Error: {e}")
+    finally:
+        gdino_service.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
