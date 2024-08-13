0e7d1c2d8fddae1f52612eb0696d019dbaa86b5d
Kajetan Rachwał <69043608+rachwalk@users.noreply.github.com>
2024-08-13 10:15:03 +0200

    feat: add grouding dino ros2 node (#81)
    * Initial commit adding interfaces and grounding dino
    
    * Update gitignore to include ros build dirs
    
    * Create README with installation instructions, reproducible install
    
    * Add launch files, finalise readme
    
    * pre-commit format
    
    * Remove unnecessary tests
    
    * Remove weights from .gitignore
    
    * Add grounding dino as poetry dependency
    
    * Update package README
    
    * Update base docs
    
    * Add missing dependancy to docs
    
    * Remove venv from .gitignore
    
    * Formatting
    
    * Add vision_msgs as dependency
    
    * Fix typos
    
    * Add automatic download of weights
    
    * Add README
    
    * Update README.md
    
    * prettier
    
    * fix build
    
    * update docs with pythonpath modification
    
    * Update src/rai_grounding_dino/README.md
    
    Co-authored-by: Bartek Boczek <22739059+boczekbartek@users.noreply.github.com>
    
    * Update src/rai_interfaces/package.xml
    
    Co-authored-by: Bartek Boczek <22739059+boczekbartek@users.noreply.github.com>
    
    * Update src/rai_interfaces/package.xml
    
    Co-authored-by: Bartek Boczek <22739059+boczekbartek@users.noreply.github.com>
    
    * Update src/rai_interfaces/package.xml
    
    Co-authored-by: Bartek Boczek <22739059+boczekbartek@users.noreply.github.com>
    
    * rm redundant file
    
    * add rosidl_generator to package xml for interfaces
    
    ---------
    
    Co-authored-by: Bartek Boczek <22739059+boczekbartek@users.noreply.github.com>

---
+++ src/rai_grounding_dino/rai_grounding_dino/boxer.py
@@ -0,0 +1,96 @@
+from os import PathLike
+from typing import Dict
+
+from cv_bridge import CvBridge
+from groundingdino.util.inference import Model
+from rclpy.time import Time
+from sensor_msgs.msg import Image
+from std_msgs.msg import Header
+from vision_msgs.msg import (
+    BoundingBox2D,
+    Detection2D,
+    ObjectHypothesis,
+    ObjectHypothesisWithPose,
+)
+
+
+class Box:
+    def __init__(self, center, size_x, size_y, phrase, confidence):
+        self.phrase = phrase
+        self.center = center
+        self.size_x = float(size_x)
+        self.size_y = float(size_y)
+        self.confidence = float(confidence)
+
+    def to_detection_msg(
+        self, class_dict: Dict[str, int], timestamp: Time
+    ) -> Detection2D:
+        detection = Detection2D()
+        detection.header = Header()
+        detection.header.stamp = timestamp
+        detection.results = []
+        hypothesis_with_pose = ObjectHypothesisWithPose()
+        hypothesis_with_pose.hypothesis = ObjectHypothesis()
+        hypothesis_with_pose.hypothesis.class_id = self.phrase
+        hypothesis_with_pose.hypothesis.score = self.confidence
+        detection.results.append(hypothesis_with_pose)
+        detection.bbox = BoundingBox2D()
+        detection.bbox.center.position.x = self.center[0]
+        detection.bbox.center.position.y = self.center[1]
+        detection.bbox.size_x = self.size_x
+        detection.bbox.size_y = self.size_y
+        return detection
+
+
+class GDBoxer:
+    def __init__(
+        self,
+        weight_path: (
+            str | PathLike
+        ) = "./src/rai_grounding_dino/GroundingDINO/weights/groundingdino_swint_ogc.pth",
+        use_cuda: bool = True,
+    ):
+        self.cfg_path = __file__.replace("boxer.py", "config.py")
+        self.weight_path = str(weight_path)
+        if not use_cuda:
+            self.model = Model(self.cfg_path, self.weight_path, device="cpu")
+        else:
+            self.model = Model(self.cfg_path, self.weight_path)
+        self.bridge = CvBridge()
+
+    def get_boxes(
+        self,
+        image_msg: Image,
+        classes: list[str],
+        box_threshold: float,
+        text_threshold: float,
+    ) -> list[Box]:
+        image = self.bridge.imgmsg_to_cv2(
+            image_msg, desired_encoding=image_msg.encoding
+        )
+
+        predictions = self.model.predict_with_classes(
+            image=image,
+            classes=classes,
+            box_threshold=box_threshold,
+            text_threshold=text_threshold,
+        )
+        packed_boxes = []
+        xyxy = predictions.xyxy
+        class_ids = predictions.class_id
+        confidences = predictions.confidence
+        if len(xyxy) == 0:
+            return []
+        else:
+            assert class_ids is not None
+            assert confidences is not None
+
+        for i in range(len(xyxy)):
+            x1, y1, x2, y2 = xyxy[i]
+            phrase = classes[class_ids[i]]
+            confidence = confidences[i]
+            center = ((x1 + x2) / 2, (y1 + y2) / 2)
+            size_x = x2 - x1
+            size_y = y2 - y1
+            packed_boxes.append(Box(center, size_x, size_y, phrase, confidence))
+        return packed_boxes
---
+++ src/rai_grounding_dino/rai_grounding_dino/talker.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+import rclpy
+from cv_bridge import CvBridge
+from rclpy.node import Node
+
+from rai_interfaces.srv import RAIGroundingDino  # CHANGE
+
+
+class GDClientExample(Node):
+    def __init__(self):
+        super().__init__(node_name="GDClientExample", parameter_overrides=[])
+        self.declare_parameter("image_path", "")
+        self.cli = self.create_client(RAIGroundingDino, "grounding_dino_classify")
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("service not available, waiting again...")
+        self.req = RAIGroundingDino.Request()
+        self.bridge = CvBridge()
+
+    def send_request(self):
+        image_path = self.get_parameter("image_path").value
+        assert isinstance(image_path, str)
+        img = cv2.imread(image_path)
+        # convert img to numpy array
+        img = np.array(img)
+        self.req.source_img = self.bridge.cv2_to_imgmsg(img, encoding="bgr8")
+        self.req.classes = "dragon , lizard , dinosaur"
+        self.req.box_threshold = 0.4
+        self.req.text_threshold = 0.4
+
+        self.future = self.cli.call_async(self.req)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_client = GDClientExample()
+    minimal_client.send_request()
+
+    while rclpy.ok():
+        rclpy.spin_once(minimal_client)
+        if minimal_client.future.done():
+            try:
+                response = minimal_client.future.result()
+            except Exception as e:
+                minimal_client.get_logger().info("Service call failed %r" % (e,))
+            else:
+                minimal_client.get_logger().info(f"{response.detections}")  # CHANGE
+            break
+
+    minimal_client.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
