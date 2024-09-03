44de34cf8d461420ad2993dddccd5b6214e3d761
Maciej Majek <maciej.majek@robotec.ai>
2024-09-03 20:34:53 +0200

    style: remove additional hash after license header

--- src/rai/rai/tools/ros/__init__.py
+++ src/rai/rai/tools/ros/__init__.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 from .cli import Ros2InterfaceTool, Ros2ServiceTool, Ros2TopicTool
 from .tools import (
