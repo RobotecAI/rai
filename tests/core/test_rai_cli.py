2dbb193d7aacd2793c04625f6225226a98ae7a30
Maciej Majek <maciej.majek@robotec.ai>
2024-09-03 20:24:49 +0200

    refactor: rai_ws() into pytest fixture

--- tests/core/test_rai_cli.py
+++ tests/core/test_rai_cli.py
@@ -1,27 +1,34 @@
 import os
+import uuid
 from pathlib import Path
-from tempfile import TemporaryDirectory
 from unittest.mock import MagicMock, patch
+
+import pytest
 
 from rai.cli.rai_cli import create_rai_ws
 
 
-def test_create_rai_ws():
-    with TemporaryDirectory() as directory:
-        # Mock ArgumentParser and its methods
-        mock_parser = MagicMock()
-        mock_args = MagicMock()
-        mock_args.name = "test_package"
-        mock_args.destination_directory = directory
-        mock_parser.parse_args.return_value = mock_args
+@pytest.fixture
+def rai_ws():
+    directory = Path("/tmp/" + str(uuid.uuid4()))
+    os.mkdir(directory)
+    mock_parser = MagicMock()
+    mock_args = MagicMock()
+    mock_args.name = "test_package"
+    mock_args.destination_directory = directory
+    mock_parser.parse_args.return_value = mock_args
 
-        # Patch argparse.ArgumentParser to return our mock
-        with patch("argparse.ArgumentParser", return_value=mock_parser):
-            create_rai_ws()
+    # Patch argparse.ArgumentParser to return our mock
+    with patch("argparse.ArgumentParser", return_value=mock_parser):
+        create_rai_ws()
 
-            whoami_directory = Path(directory) / "test_package_whoami"
+    return directory
 
-            assert os.path.exists(whoami_directory), "Description folder is missing"
 
-            description_files = os.listdir(whoami_directory / "description")
-            assert "robot_constitution.txt" in description_files
+def test_create_rai_ws(rai_ws: Path):
+    whoami_directory = Path(rai_ws) / "test_package_whoami"
+
+    assert os.path.exists(whoami_directory), "Description folder is missing"
+
+    description_files = os.listdir(whoami_directory / "description")
+    assert "robot_constitution.txt" in description_files
