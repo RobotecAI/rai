c17e0934df61598a5aac2a2ebf2c8c1e90c09999
rachwalk <kajetan.rachwal@robotec.ai>
2024-09-09 18:34:40 +0200

    chore: cleanup, make service name constant

--- src/rai_extensions/rai_grounding_dino/rai_grounding_dino/tools.py
+++ src/rai_extensions/rai_grounding_dino/rai_grounding_dino/tools.py
@@ -1,7 +1,19 @@
-import pdb
+# Copyright (C) 2024 Robotec.AI
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import Optional, Type
 
-import cv2
 import rclpy
 import sensor_msgs.msg
 from langchain_core.pydantic_v1 import Field
@@ -17,9 +29,6 @@ class Ros2GetDetectionInput(Ros2BaseInput):
     camera_topic: str = Field(
         ...,
         description="Ros2 topic for the camera image containing image to run detection on.",
-    )
-    service_name: str = Field(
-        ..., description="Name of the service to perform depth analysis"
     )
     object_name: list[str] = Field(
         ..., description="Natural language name of the objects to detect"
@@ -49,7 +58,6 @@ class GetDetectionTool(Ros2BaseTool):
     def _run(
         self,
         camera_topic: str,
-        service_name: str,
         object_names: list[str],
     ):
         assert hasattr(self.node, "get_raw_message_from_topic")
@@ -60,7 +68,7 @@ class GetDetectionTool(Ros2BaseTool):
         else:
             raise Exception("Received wrong message")
 
-        cli = self.node.create_client(RAIGroundingDino, service_name)
+        cli = self.node.create_client(RAIGroundingDino, "grounding_dino_classify")
         while not cli.wait_for_service(timeout_sec=1.0):
             self.node.get_logger().info("service not available, waiting again...")
         req = RAIGroundingDino.Request()
