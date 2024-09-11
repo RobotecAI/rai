5e8360f274b91b89699ac0b8af46dc62105f169e
Kajetan Rachwał <69043608+rachwalk@users.noreply.github.com>
2024-09-11 11:46:48 +0200

    feat: support multiple shells in the setup script (#216)

--- setup_shell.sh
+++ setup_shell.sh
@@ -6,7 +6,27 @@
 
 # Suppress ShellCheck warning about not following external file
 # shellcheck disable=SC1091
-. install/setup.bash
+
+case "$SHELL" in
+    *bash)
+        . install/setup.bash
+        echo "Sourced bash install"
+        ;;
+    *zsh)
+        . install/setup.zsh
+        echo "Sourced zsh install"
+        ;;
+    *fish)
+        echo "fish is not supported"
+        ;;
+    *sh)
+        . install/setup.sh
+        echo "Sourced sh install."
+        ;;
+    *)
+        echo "Unknown shell: $0"
+        ;;
+esac
 
 export PYTHONPATH
 PYTHONPATH="$(dirname "$(dirname "$(poetry run which python)")")/lib/python$(poetry run python --version | awk '{print $2}' | cut -d. -f1,2)/site-packages:$PYTHONPATH"
