b1a488881365f7ad209f06aed04497c3e278c208
Kacper Dąbrowski <kn1ck3d@gmail.com>
2024-09-16 12:40:28 +0200

    Update src/rai_extensions/rai_nomad/rai_nomad/nomad.py
    Co-authored-by: Maciej Majek <46171033+maciejmajek@users.noreply.github.com>

--- src/rai_extensions/rai_nomad/rai_nomad/nomad.py
+++ src/rai_extensions/rai_nomad/rai_nomad/nomad.py
@@ -12,6 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# The contents of this file include code from (https://github.com/robodhruv/visualnav-transformer) as well as fork (https://github.com/RobotecAI/visualnav-transformer-ros2) with the following license:
+# MIT License
+
+# Copyright (c) 2023 Dhruv Shah, Ajay Sridhar, Nitish Dashora, Kyle Stachowicz, Kevin Black, Noriaki Hirose, Sergey Levine
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 
 import os
 from typing import Tuple
