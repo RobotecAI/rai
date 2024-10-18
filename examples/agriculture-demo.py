3825ee494331fd012b01e5a422632c1192fadc3f
Maciej Majek <maciej.majek@robotec.ai>
2024-10-18 21:41:51 +0200

    feat: self contained demo file

--- examples/agriculture-demo.py
+++ examples/agriculture-demo.py
@@ -15,6 +15,9 @@
 
 
 import argparse
+import subprocess
+import threading
+import time
 
 import rclpy
 
@@ -27,6 +30,41 @@ from rai.tools.ros.native import (
     Ros2ShowMsgInterfaceTool,
 )
 from rai.tools.time import WaitForSecondsTool
+
+
+def mock_behavior_tree(tractor_number: int):
+    """
+    This is a mock of behavior tree that simulates scenario where the tractor stops
+    due to an unexpected situation and calls the RaiNode to decide what to do.
+    """
+
+    while True:
+        output = subprocess.check_output(
+            [
+                "ros2",
+                "service",
+                "call",
+                f"/tractor{tractor_number}/current_state",
+                "std_srvs/srv/Trigger",
+                "{}",
+            ]
+        )
+        print(output.decode("utf-8"))
+        if "STOPPED" in output.decode("utf-8"):
+            print("The tractor has stopped. Calling RaiNode to decide what to do.")
+            action_output = subprocess.check_output(
+                [
+                    "ros2",
+                    "action",
+                    "send_goal",
+                    "-f",
+                    "/perform_task",
+                    "rai_interfaces/action/Task",
+                    "{priority: 10, description: '', task: 'Anomaly detected. Please decide what to do.'}",
+                ]
+            )
+            print(action_output.decode("utf-8"))
+        time.sleep(5)
 
 
 def main():
@@ -93,6 +131,9 @@ def main():
             Ros2GenericServiceCaller,
         ],
     )
+
+    thread = threading.Thread(target=mock_behavior_tree, args=(tractor_number,))
+    thread.start()
 
     node.spin()
     rclpy.shutdown()
