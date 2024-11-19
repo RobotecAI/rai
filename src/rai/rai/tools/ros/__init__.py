1dd21d335c86070e8b8c69b2eb66222db0dcfef3
Maciej Majek <46171033+maciejmajek@users.noreply.github.com>
2024-11-19 15:59:22 +0100

    style: remove additional hash from license notice (#311)

--- src/rai/rai/tools/ros/__init__.py
+++ src/rai/rai/tools/ros/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
 
 from .cli import Ros2InterfaceTool, Ros2ServiceTool, Ros2TopicTool
 from .native import Ros2BaseInput, Ros2BaseTool
