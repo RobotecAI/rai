b0069ffae8295d7b1ac98e08008e674f73c767e7
Bartłomiej Boczek <bartlomiej.boczek@robotec.ai>
2024-11-22 08:40:10 +0100

    feat: integrate with `rai_state_logs`

--- src/rai/rai/utils/ros.py
+++ src/rai/rai/utils/ros.py
@@ -13,48 +13,8 @@
 # limitations under the License.
 
 
-from collections import deque
 from dataclasses import dataclass, field
-from typing import Deque, Dict, List, Optional, Tuple
-
-from langchain_core.language_models import BaseChatModel
-from langchain_core.prompts import ChatPromptTemplate
-
-
-class RosoutBuffer:
-    def __init__(self, llm: BaseChatModel, bufsize: int = 100) -> None:
-        self.bufsize = bufsize
-        self._buffer: Deque[str] = deque()
-        self.template = ChatPromptTemplate.from_messages(
-            [
-                (
-                    "system",
-                    "Shorten the following log keeping its format - for example merge similar or repeating lines",
-                ),
-                ("human", "{rosout}"),
-            ]
-        )
-        llm = llm
-        self.llm = self.template | llm
-
-    def clear(self):
-        self._buffer.clear()
-
-    def append(self, line: str):
-        self._buffer.append(line)
-        if len(self._buffer) > self.bufsize:
-            self._buffer.popleft()
-
-    def get_raw_logs(self, last_n: int = 30) -> str:
-        return "\n".join(list(self._buffer)[-last_n:])
-
-    def summarize(self):
-        if len(self._buffer) == 0:
-            return "No logs"
-        buffer = self.get_raw_logs()
-        self.clear()
-        response = self.llm.invoke({"rosout": buffer})
-        return str(response.content)
+from typing import Dict, List, Optional, Tuple
 
 
 @dataclass
