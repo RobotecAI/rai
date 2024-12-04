3a8e7658b82df4cdb01d8194a2ed9fa2371aec06
Maciej Majek <maciej.majek@robotec.ai>
2024-12-04 15:42:16 +0100

    chore: print minimized tool output

--- src/rai/rai/agents/tool_runner.py
+++ src/rai/rai/agents/tool_runner.py
@@ -69,7 +69,9 @@ class ToolRunner(RunnableCallable):
                 ts = time.perf_counter()
                 output = self.tools_by_name[call["name"]].invoke(call, config)  # type: ignore
                 te = time.perf_counter() - ts
-                self.logger.info(f"Tool {call['name']} completed in {te:.2f} seconds.")
+                self.logger.info(
+                    f"Tool {call['name']} completed in {te:.2f} seconds. Tool output: {str(output.content)[:100]}{'...' if len(str(output.content)) > 100 else ''}"
+                )
                 self.logger.debug(
                     f"Tool {call['name']} output: \n\n{str(output.content)}"
                 )
