a9f6067a1fefd6fee28fe74606871951b74db101
Maciej Majek <maciej.majek@robotec.ai>
2025-01-17 17:49:56 +0100

    feat: Implement ROS2ARIConnector + tests

---
+++ src/rai/rai/communication/ros2/connectors.py
@@ -0,0 +1,111 @@
+# Copyright (C) 2024 Robotec.AI
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import threading
+from typing import Any, Callable, Optional, TypedDict
+
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+
+from rai.communication.ari_connector import ARIConnector, ARIMessage
+from rai.communication.ros2.api import ROS2ActionAPI, ROS2ServiceAPI, ROS2TopicAPI
+
+
+class ROS2ARIMessageMetadata(TypedDict):
+    msg_type: str
+    qos_profile: Optional[QoSProfile]
+    auto_qos_matching: bool
+
+
+class ROS2ARIMessage(ARIMessage):
+    metadata: ROS2ARIMessageMetadata
+
+
+class ROS2ARIConnector(ARIConnector):
+    def __init__(self, node_name: str = "rai_ros2_ari_connector"):
+        super().__init__()
+        self._node = Node(node_name)
+        self._topic_api = ROS2TopicAPI(self._node)
+        self._service_api = ROS2ServiceAPI(self._node)
+        self._actions_api = ROS2ActionAPI(self._node)
+
+        self._executor = MultiThreadedExecutor()
+        self._executor.add_node(self._node)
+        self._thread = threading.Thread(target=self._executor.spin)
+        self._thread.start()
+
+    def send_message(self, message: ARIMessage, target: str):
+        if not isinstance(message, ROS2ARIMessage):
+            raise ValueError("Message must be of type ROS2ARIMessage")
+
+        self._topic_api.publish(
+            topic=target, msg_content=message.payload, **message.metadata
+        )
+
+    def receive_message(self, source: str, timeout_sec: float = 1.0) -> ARIMessage:
+        msg = self._topic_api.receive(topic=source, timeout_sec=timeout_sec)
+        return ARIMessage(
+            payload=msg, metadata={"msg_type": str(type(msg)), "topic": source}
+        )
+
+    def service_call(
+        self, message: ARIMessage, target: str, timeout_sec: float = 1.0
+    ) -> ARIMessage:
+        if not isinstance(message, ROS2ARIMessage):
+            raise ValueError("Message must be of type ROS2ARIMessage")
+
+        msg = self._service_api.call_service(
+            service_name=target,
+            service_type=message.metadata["msg_type"],
+            request=message.payload,
+            timeout_sec=timeout_sec,
+        )
+        return ARIMessage(
+            payload=msg, metadata={"msg_type": str(type(msg)), "service": target}
+        )
+
+    def start_action(
+        self,
+        action_data: Optional[ARIMessage],
+        target: str,
+        on_feedback: Callable[[Any], None] = lambda _: None,
+        on_done: Callable[[Any], None] = lambda _: None,
+        timeout_sec: float = 1.0,
+    ) -> str:
+        if not isinstance(action_data, ROS2ARIMessage):
+            raise ValueError("Action data must be of type ROS2ARIMessage")
+
+        accepted, handle = self._actions_api.send_goal(
+            action_name=target,
+            action_type=action_data.metadata["msg_type"],
+            goal=action_data.payload,
+            timeout_sec=timeout_sec,
+            feedback_callback=on_feedback,
+            done_callback=on_done,
+        )
+        if not accepted:
+            raise RuntimeError("Action goal was not accepted")
+        return handle
+
+    def terminate_action(self, action_handle: str):
+        self._actions_api.terminate_goal(action_handle)
+
+    def shutdown(self):
+        self._cleanup()
+
+    def _cleanup(self):
+        self._executor.shutdown()
+        self._thread.join()
+        self._node.destroy_node()
---
+++ tests/communication/ros2/test_connectors.py
@@ -0,0 +1,193 @@
+# Copyright (C) 2024 Robotec.AI
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+from typing import Any, List
+
+import pytest
+from std_msgs.msg import String
+from std_srvs.srv import SetBool
+
+from rai.communication.ros2.connectors import ROS2ARIConnector, ROS2ARIMessage
+
+from .helpers import ActionServer_ as ActionServer
+from .helpers import (
+    MessagePublisher,
+    MessageReceiver,
+    ServiceServer,
+    multi_threaded_spinner,
+    ros_setup,
+    shutdown_executors_and_threads,
+)
+
+ros_setup  # type: ignore prevents pytest from complaining about unused fixture
+
+
+def test_ros2ari_connector_send_message(
+    ros_setup: None, request: pytest.FixtureRequest
+):
+    topic_name = f"{request.node.originalname}_topic"  # type: ignore
+    message_receiver = MessageReceiver(topic_name)
+    executors, threads = multi_threaded_spinner([message_receiver])
+    connector = ROS2ARIConnector()
+    try:
+        message = ROS2ARIMessage(
+            payload={"data": "Hello, world!"},
+            metadata={"msg_type": "std_msgs/msg/String"},
+        )
+        connector.send_message(message, target=topic_name)
+        time.sleep(1)  # wait for the message to be received
+        assert message_receiver.received_messages == [String(data="Hello, world!")]
+    finally:
+        connector.shutdown()
+        shutdown_executors_and_threads(executors, threads)
+
+
+@pytest.mark.xfail(
+    reason="Test expected to fail: ROS2 node discovery is asynchronous and the current implementation "
+    "doesn't wait for topic discovery. "
+    "TODO: Implement a proper discovery mechanism with timeout "
+    "to ensure reliable topic communication."
+)
+def test_ros2ari_connector_receive_message(
+    ros_setup: None, request: pytest.FixtureRequest
+):
+    topic_name = f"{request.node.originalname}_topic"  # type: ignore
+    message_publisher = MessagePublisher(topic_name)
+    executors, threads = multi_threaded_spinner([message_publisher])
+    connector = ROS2ARIConnector()
+    try:
+        message = connector.receive_message(topic_name, timeout_sec=1.0)
+        assert message.payload == String(data="Hello, ROS2!")
+    finally:
+        connector.shutdown()
+        shutdown_executors_and_threads(executors, threads)
+
+
+def test_ros2ari_connector_service_call(
+    ros_setup: None, request: pytest.FixtureRequest
+):
+    service_name = f"{request.node.originalname}_service"  # type: ignore
+    message_receiver = ServiceServer(service_name)
+    executors, threads = multi_threaded_spinner([message_receiver])
+    connector = ROS2ARIConnector()
+    try:
+        message = ROS2ARIMessage(
+            payload={"data": True},
+            metadata={"msg_type": "std_srvs/srv/SetBool"},
+        )
+        response = connector.service_call(message, target=service_name)
+        assert response.payload == SetBool.Response(
+            success=True, message="Test service called"
+        )
+    finally:
+        connector.shutdown()
+        shutdown_executors_and_threads(executors, threads)
+
+
+def test_ros2ari_connector_send_goal(ros_setup: None, request: pytest.FixtureRequest):
+    action_name = f"{request.node.originalname}_action"  # type: ignore
+    action_server = ActionServer(action_name)
+    executors, threads = multi_threaded_spinner([action_server])
+    connector = ROS2ARIConnector()
+    try:
+        message = ROS2ARIMessage(
+            payload={},
+            metadata={"msg_type": "nav2_msgs/action/NavigateToPose"},
+        )
+        handle = connector.start_action(action_data=message, target=action_name)
+        assert handle is not None
+    finally:
+        connector.shutdown()
+        shutdown_executors_and_threads(executors, threads)
+
+
+def test_ros2ari_connector_send_goal_and_terminate_action(
+    ros_setup: None, request: pytest.FixtureRequest
+):
+    action_name = f"{request.node.originalname}_action"  # type: ignore
+    action_server = ActionServer(action_name)
+    executors, threads = multi_threaded_spinner([action_server])
+    connector = ROS2ARIConnector()
+    feedbacks: List[Any] = []
+    try:
+        message = ROS2ARIMessage(
+            payload={},
+            metadata={"msg_type": "nav2_msgs/action/NavigateToPose"},
+        )
+        handle = connector.start_action(
+            action_data=message,
+            target=action_name,
+            on_feedback=lambda feedback: feedbacks.append(feedback),
+        )
+        assert handle is not None
+        feedbacks_before = feedbacks
+        connector.terminate_action(handle)
+        feedbacks_after = feedbacks
+        assert len(feedbacks_after) == len(feedbacks_before)
+    finally:
+        connector.shutdown()
+        shutdown_executors_and_threads(executors, threads)
+
+
+def test_ros2ari_connector_send_goal_erronous_callback(
+    ros_setup: None, request: pytest.FixtureRequest
+):
+    action_name = f"{request.node.originalname}_action"  # type: ignore
+    action_server = ActionServer(action_name)
+    executors, threads = multi_threaded_spinner([action_server])
+    connector = ROS2ARIConnector()
+    try:
+        message = ROS2ARIMessage(
+            payload={},
+            metadata={"msg_type": "nav2_msgs/action/NavigateToPose"},
+        )
+        handle = connector.start_action(
+            action_data=message,
+            target=action_name,
+            on_feedback=lambda feedback: 1 / 0,
+        )
+        assert handle is not None
+    finally:
+        connector.shutdown()
+        shutdown_executors_and_threads(executors, threads)
+
+
+@pytest.mark.skip(
+    reason="""
+                  This test does not allow pytest to exit.
+                  TODO: Decide whether still working callbacks should
+                  be cancelled when shutting down"""
+)
+def test_ros2ari_connector_send_goal_blocking_callback(
+    ros_setup: None, request: pytest.FixtureRequest
+):
+    action_name = f"{request.node.originalname}_action"  # type: ignore
+    action_server = ActionServer(action_name)
+    executors, threads = multi_threaded_spinner([action_server])
+    connector = ROS2ARIConnector()
+    try:
+        message = ROS2ARIMessage(
+            payload={},
+            metadata={"msg_type": "nav2_msgs/action/NavigateToPose"},
+        )
+        handle = connector.start_action(
+            action_data=message,
+            target=action_name,
+            on_feedback=lambda feedback: time.sleep(100),
+        )
+        assert handle is not None
+    finally:
+        connector.shutdown()
+        shutdown_executors_and_threads(executors, threads)
