10060e2f33fd2f62bac2173e63023b8c1aad2d6b
Magdalena Kotynia <magdalena.kotynia@robotec.ai>
2025-02-18 13:48:02 +0100

    chore: replaced link to PR with other PR

--- src/rai_sim/rai_sim/o3de/o3de_connector.py
+++ src/rai_sim/rai_sim/o3de/o3de_connector.py
@@ -107,7 +107,7 @@ class O3DExROS2Connector(SimulationConnector[O3DExROS2SimulationConfig]):
             msg_type="gazebo_msgs/srv/GetWorldProperties",
         )
         # NOTE (mkotynia) There is a bug in the gazebo_msgs/srv/GetWorldProperties service - payload.success is not set to True even if the service call is successful. It was reported to Kacper Dąbrowski and he is going to fix it.
-        # PR fixing the bug: https://github.com/RobotecAI/o3de-extras/pull/66
+        # PR fixing the bug: https://github.com/o3de/o3de-extras/pull/828
         # TODO (mkotynia) uncomment check if response.payload.success when the bug is fixed and remove workaround check if response.payload.model_names.
 
         # if response.payload.success:
