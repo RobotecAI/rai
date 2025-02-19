f0e59741b4f285912a56779823470d105c2eb2a1
jmatejcz <jakub.matejczyk@robotec.ai>
2025-02-19 09:59:07 +0100

    style: added license

--- src/rai_bench/o3de_test_bench/main.py
+++ src/rai_bench/o3de_test_bench/main.py
@@ -1,3 +1,17 @@
+# Copyright (C) 2025 Robotec.AI
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 ########### EXAMPLE USAGE ###########
 import rclpy
 import logging
@@ -42,8 +56,6 @@ class GrabCarrotTask(Task):
         scene_state = engine_connector.get_scene_state()
         entities = scene_state.entities
 
-        print(entities)
-        print(initial_scene_setup)
         for entity in entities:
             if entity.prefab_name == "carrot":  # check all carrots
                 for ini_ent in initial_scene_setup.entities:
@@ -76,7 +88,7 @@ class GrabCarrotTask(Task):
             print("No objects to manipulate, returning 1...")
             return 1
         else:
-            result = corrected_objects + unchanged_correct / (
+            result = (corrected_objects + unchanged_correct) / (
                 corrected_objects
                 + misplaced_objects
                 + unchanged_correct
@@ -198,11 +210,11 @@ if __name__ == "__main__":
     )
     # combine different scene configs with the tasks to create various scenarios
     scenarios = [
-        Scenario(task=GrabCarrotTask(), scene_config=one_carrot_scene_config),
+        # Scenario(task=GrabCarrotTask(), scene_config=one_carrot_scene_config),
         Scenario(task=GrabCarrotTask(), scene_config=multiple_carrot_scene_config),
-        Scenario(task=GrabCarrotTask(), scene_config=red_cubes_scene_config),
-        Scenario(task=RedCubesTask(), scene_config=one_carrot_scene_config),
-        Scenario(task=RedCubesTask(), scene_config=red_cubes_scene_config),
+        # Scenario(task=GrabCarrotTask(), scene_config=red_cubes_scene_config),
+        # Scenario(task=RedCubesTask(), scene_config=one_carrot_scene_config),
+        # Scenario(task=RedCubesTask(), scene_config=red_cubes_scene_config),
     ]
     # define logger
     log_file = "src/rai_bench/o3de_test_bench/benchamrk_agent.log"
