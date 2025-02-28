413291a5024b2d18146de35987a41e10fbff4002
jmatejcz <jakub.matejczyk@robotec.ai>
2025-02-28 10:29:18 +0100

    feat: add helper funtions to find clusters in objects

--- src/rai_bench/rai_bench/o3de_test_bench/tasks/manipulation_task.py
+++ src/rai_bench/rai_bench/o3de_test_bench/tasks/manipulation_task.py
@@ -1,8 +1,16 @@
-from rai_bench.benchmark_model import Task  # type: ignore
+from rai_bench.benchmark_model import EntitiesMismatchException, Task  # type: ignore
+from rai_sim.o3de.o3de_bridge import SimulationBridge  # type: ignore
+from rai_sim.simulation_bridge import SimulationConfig, SpawnedEntity  # type: ignore
+import logging
+from typing import Union
+
+from rclpy.impl.rcutils_logger import RcutilsLogger
+
+loggers_type = Union[RcutilsLogger, logging.Logger]
 
 
 class ManipulationTask(Task):
-    def __init__(self, logger=None):
+    def __init__(self, logger: loggers_type | None = None):
         super().__init__(logger)
         self.initially_misplaced_now_correct = 0
         self.initially_misplaced_still_incorrect = 0
