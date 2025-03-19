0556bbda9d8dd69441b6a9b02d03de6446807688
Magdalena Kotynia <magdalena.kotynia@robotec.ai>
2025-03-19 14:14:40 +0100

    refactor: created TaskResult structure to make storing task results more maintainable

--- src/rai_bench/rai_bench/tool_calling_agent_bench/agent_bench.py
+++ src/rai_bench/rai_bench/tool_calling_agent_bench/agent_bench.py
@@ -16,8 +16,10 @@ import csv
 import logging
 import time
 import uuid
-from typing import Any, Dict, List, Sequence
+from typing import Iterator, List, Sequence, Tuple
+from uuid import UUID
 
+from pydantic import BaseModel
 from rai.messages.multimodal import HumanMultimodalMessage
 
 from rai_bench.tool_calling_agent_bench.agent_tasks_interfaces import (
@@ -26,6 +28,14 @@ from rai_bench.tool_calling_agent_bench.agent_tasks_interfaces import (
 from rai_bench.tool_calling_agent_bench.scores_tracing import ScoreTracingHandler
 
 loggers_type = logging.Logger
+
+
+class TaskResult(BaseModel):
+    task: str
+    success: bool
+    errors: List[str]
+    total_time: float
+    run_id: UUID
 
 
 class ToolCallingAgentBenchmark:
@@ -39,17 +49,11 @@ class ToolCallingAgentBenchmark:
         logger: loggers_type | None = None,
         results_filename: str = "agent_benchmark_results.csv",
     ) -> None:
-        self._tasks = enumerate(iter(tasks))
+        self._tasks: Iterator[Tuple[int, ToolCallingAgentTask]] = enumerate(iter(tasks))
         self.num_tasks = len(tasks)
-        self.tasks_results: List[Dict[str, Any]] = []
+        self.task_results: List[TaskResult] = []
         self.results_filename = results_filename
-        self.fieldnames = [
-            "task",
-            "success",
-            "errors",
-            "total_time",
-            "run_id",
-        ]
+        self.fieldnames = [field for field in TaskResult.__annotations__.keys()]
         self._initialize_results_file()
         self.score_tracing_handler = ScoreTracingHandler()
         if logger:
@@ -91,25 +95,25 @@ class ToolCallingAgentBenchmark:
                 f"TASK SUCCESS: {result.success}, TOTAL TIME: {total_time:.3f}"
             )
 
-            task_result: Dict[str, Any] = {
-                "task": task.get_prompt(),
-                "success": result.success,
-                "errors": "; ".join(result.errors) if result.errors else "",
-                "total_time": total_time,
-                "run_id": run_id,
-            }
-            self.tasks_results.append(task_result)
+            task_result = TaskResult(
+                task=task.get_prompt(),
+                success=result.success,
+                errors=result.errors if result.errors else [],
+                total_time=total_time,
+                run_id=run_id,
+            )
+            self.task_results.append(task_result)
             self._save_task_result_to_csv(task_result)
         except StopIteration:
             print("No more scenarios left to run.")
 
-    def _save_task_result_to_csv(self, result: Dict[str, Any]) -> None:
+    def _save_task_result_to_csv(self, result: TaskResult) -> None:
         """Save a single task result to the CSV file."""
         with open(
             self.results_filename, mode="a", newline="", encoding="utf-8"
         ) as file:
             writer = csv.DictWriter(file, fieldnames=self.fieldnames)
-            writer.writerow(result)
+            writer.writerow(result.model_dump())
 
     def _initialize_results_file(self):
         """Initialize the CSV file with headers."""
