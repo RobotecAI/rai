38758c63b5fa0f8bf04bcb1064ced9f6b80b373f
Magdalena Kotynia <magdalena.kotynia@robotec.ai>
2025-03-25 16:58:03 +0100

    docs: apply numpy style docstrings in the mocked_tools.py

--- src/rai_bench/rai_bench/tool_calling_agent_bench/mocked_tools.py
+++ src/rai_bench/rai_bench/tool_calling_agent_bench/mocked_tools.py
@@ -16,6 +16,7 @@ from typing import List, Tuple
 from unittest.mock import MagicMock
 
 import numpy as np
+import numpy.typing as npt
 from rai.communication.ros2.connectors import ROS2ARIConnector
 from rai.communication.ros2.messages import ROS2ARIMessage
 from rai.messages import MultimodalArtifact, preprocess_image
@@ -36,7 +37,13 @@ class MockGetROS2TopicsNamesAndTypesTool(GetROS2TopicsNamesAndTypesTool):
     mock_topics_names_and_types: list[str]
 
     def _run(self) -> str:
-        """Return the mock topics and types instead of fetching from ROS2."""
+        """Mocked method that returns the mock topics and types instead of fetching from ROS2.
+
+        Returns
+        -------
+        str
+            Mocked output of 'get_ros2_topics_names_and_types' tool.
+        """
         return "\n".join(self.mock_topics_names_and_types)
 
 
@@ -47,6 +54,25 @@ class MockGetROS2ImageTool(GetROS2ImageTool):
     def _run(
         self, topic: str, timeout_sec: float = 1.0
     ) -> Tuple[str, MultimodalArtifact]:
+        """Method that returns a mock black image if the passed topic is correct.
+
+        Parameters
+        ----------
+        topic : str
+            Topic with the image
+        timeout_sec : float, optional
+            Timeout in seconds, by default 1.0
+
+        Returns
+        -------
+        Tuple[str, MultimodalArtifact]
+            Message from the tool and the image.
+
+        Raises
+        ------
+        ValueError
+            If the passed topic is not correct.
+        """
         if topic not in self.expected_topics:
             raise ValueError(
                 f"Topic {topic} is not available within {timeout_sec} seconds. Check if the topic exists."
@@ -57,8 +83,14 @@ class MockGetROS2ImageTool(GetROS2ImageTool):
         )  # type: ignore
 
     @staticmethod
-    def generate_mock_image():
-        """Generate a blank black mock image (480x640, RGB)."""
+    def generate_mock_image() -> npt.NDArray[np.uint8]:
+        """Generates a blank black image (480x640, RGB).
+
+        Returns
+        -------
+        npt.NDArray[np.uint8]
+            Blank black image
+        """
         height, width = 480, 640
         blank_image = np.zeros((height, width, 3), dtype=np.uint8)
         return blank_image
@@ -69,6 +101,23 @@ class MockReceiveROS2MessageTool(ReceiveROS2MessageTool):
     expected_topics: List[str]
 
     def _run(self, topic: str) -> str:
+        """Method that returns a mock message if the passed topic is correct.
+
+        Parameters
+        ----------
+        topic : str
+            Topic to receive the message from
+
+        Returns
+        -------
+        str
+            Message from the tool
+
+        Raises
+        ------
+        ValueError
+            If the passed topic is not correct.
+        """
         if topic not in self.expected_topics:
             raise ValueError(
                 f"Topic {topic} is not available within 1.0 seconds. Check if the topic exists."
@@ -83,6 +132,21 @@ class MockMoveToPointTool(MoveToPointTool):
     connector: ROS2ARIConnector = MagicMock(spec=ROS2ARIConnector)
 
     def _run(self, x: float, y: float, z: float, task: str) -> str:
+        """Method that return a mock message with the end effector position.
+
+        Parameters
+        ----------
+        x : float
+        y : float
+        z : float
+        task : str
+            Task to perform.
+
+        Returns
+        -------
+        str
+            Message from the tool
+        """
         return f"End effector successfully positioned at coordinates ({x:.2f}, {y:.2f}, {z:.2f}). Note: The status of object interaction (grab/drop) is not confirmed by this movement."
 
 
@@ -98,7 +162,19 @@ class MockGetObjectPositionsTool(GetObjectPositionsTool):
     get_grabbing_point_tool: GetGrabbingPointTool = MagicMock(spec=GetGrabbingPointTool)
     mock_objects: dict[str, List[dict[str, float]]]
 
-    def _run(self, object_name: str):
+    def _run(self, object_name: str) -> str:
+        """Method that returns a mock message with the object positions if the object_name is present in the mock_objects dictionary.
+
+        Parameters
+        ----------
+        object_name : str
+            Name of the object to get the positions of
+
+        Returns
+        -------
+        str
+            Message from the tool
+        """
         expected_positions = self.mock_objects.get(object_name, [])
         print(f"Expected positions: {expected_positions}")
         if len([expected_positions]) == 0:
