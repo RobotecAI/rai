0d287f1f45ed3c40fd140907cc4ba698f7676445
Maciej Majek <maciej.majek@robotec.ai>
2025-03-28 15:25:59 +0100

    fix: ros2 action tool suite

--- src/rai_core/rai/tools/ros2/actions.py
+++ src/rai_core/rai/tools/ros2/actions.py
@@ -32,16 +32,49 @@ from pydantic import BaseModel, Field
 from rai.communication.ros2.connectors import ROS2ARIConnector, ROS2ARIMessage
 from rai.tools.ros2.base import BaseROS2Tool, BaseROS2Toolkit
 
+internal_action_id_mapping: Dict[str, str] = {}
+action_results_store: Dict[str, Any] = {}
+action_results_store_lock: Lock = Lock()
+action_feedbacks_store: Dict[str, List[Any]] = defaultdict(list)
+action_feedbacks_store_lock: Lock = Lock()
+
+
+def get_internal_action_id_mapping():
+    return internal_action_id_mapping
+
+
+def get_action_results_store():
+    return action_results_store
+
+
+def get_action_results_store_lock():
+    return action_results_store_lock
+
+
+def get_action_feedbacks_store():
+    return action_feedbacks_store
+
+
+def get_action_feedbacks_store_lock():
+    return action_feedbacks_store_lock
+
 
 class ROS2ActionToolkit(BaseROS2Toolkit):
     name: str = "ros2_action"
     description: str = "A toolkit for ROS2 actions"
 
-    action_results_store: Dict[str, Any] = {}
-    action_results_store_lock: Lock = Lock()
-    action_feedbacks_store: Dict[str, List[Any]] = defaultdict(list)
-    action_feedbacks_store_lock: Lock = Lock()
-    internal_action_id_mapping: Dict[str, str] = {}
+    action_results_store: Dict[str, Any] = Field(
+        default_factory=get_action_results_store
+    )
+    action_results_store_lock: Lock = Field(
+        default_factory=get_action_results_store_lock
+    )
+    action_feedbacks_store: Dict[str, List[Any]] = Field(
+        default_factory=get_action_feedbacks_store
+    )
+    action_feedbacks_store_lock: Lock = Field(
+        default_factory=get_action_feedbacks_store_lock
+    )
 
     def get_tools(self) -> List[BaseTool]:
         return [
@@ -64,18 +97,9 @@ class ROS2ActionToolkit(BaseROS2Toolkit):
                 readable=self.readable,
                 writable=self.writable,
                 forbidden=self.forbidden,
-                action_feedbacks_store=self.action_feedbacks_store,
-                action_feedbacks_store_lock=self.action_feedbacks_store_lock,
-                internal_action_id_mapping=self.internal_action_id_mapping,
             ),
-            GetROS2ActionResultTool(
-                action_results_store=self.action_results_store,
-                action_results_store_lock=self.action_results_store_lock,
-                internal_action_id_mapping=self.internal_action_id_mapping,
-            ),
-            GetROS2ActionIDsTool(
-                internal_action_id_mapping=self.internal_action_id_mapping
-            ),
+            GetROS2ActionResultTool(),
+            GetROS2ActionIDsTool(),
             GetROS2ActionsNamesAndTypesTool(
                 connector=self.connector,
                 readable=self.readable,
@@ -88,9 +112,9 @@ class ROS2ActionToolkit(BaseROS2Toolkit):
         with self.action_feedbacks_store_lock:
             self.action_feedbacks_store[action_id].append(feedback)
 
-    def _generic_on_done_callback(self, action_id: str, result: Any) -> None:
+    def _generic_on_done_callback(self, action_id: str, future: Any) -> None:
         with self.action_results_store_lock:
-            self.action_results_store[action_id] = result
+            self.action_results_store[action_id] = future.result().result
 
 
 class GetROS2ActionsNamesAndTypesToolInput(BaseModel):
@@ -140,7 +164,9 @@ class StartROS2ActionTool(BaseROS2Tool):
     connector: ROS2ARIConnector
     feedback_callback: Callable[[Any, str], None] = lambda _, __: None
     on_done_callback: Callable[[Any, str], None] = lambda _, __: None
-    internal_action_id_mapping: Dict[str, str] = {}
+    internal_action_id_mapping: Dict[str, str] = Field(
+        default_factory=get_internal_action_id_mapping
+    )
     name: str = "start_ros2_action"
     description: str = "Start a ROS2 action"
     args_schema: Type[StartROS2ActionToolInput] = StartROS2ActionToolInput
@@ -172,9 +198,15 @@ class GetROS2ActionFeedbackTool(BaseROS2Tool):
     description: str = "Get the feedback of a ROS2 action by its action ID"
     args_schema: Type[GetROS2ActionFeedbackToolInput] = GetROS2ActionFeedbackToolInput
 
-    action_feedbacks_store: Dict[str, List[Any]]
-    action_feedbacks_store_lock: Lock
-    internal_action_id_mapping: Dict[str, str] = {}
+    action_feedbacks_store: Dict[str, List[Any]] = Field(
+        default_factory=get_action_feedbacks_store
+    )
+    action_feedbacks_store_lock: Lock = Field(
+        default_factory=get_action_feedbacks_store_lock
+    )
+    internal_action_id_mapping: Dict[str, str] = Field(
+        default_factory=get_internal_action_id_mapping
+    )
 
     def _run(self, action_id: str) -> str:
         with self.action_feedbacks_store_lock:
@@ -195,9 +227,15 @@ class GetROS2ActionResultTool(BaseTool):
     description: str = "Get the result of a ROS2 action by its id"
     args_schema: Type[GetROS2ActionResultToolInput] = GetROS2ActionResultToolInput
 
-    action_results_store: Dict[str, Any]
-    action_results_store_lock: Lock
-    internal_action_id_mapping: Dict[str, str] = {}
+    action_results_store: Dict[str, Any] = Field(
+        default_factory=get_action_results_store
+    )
+    action_results_store_lock: Lock = Field(
+        default_factory=get_action_results_store_lock
+    )
+    internal_action_id_mapping: Dict[str, str] = Field(
+        default_factory=get_internal_action_id_mapping
+    )
 
     def _run(self, action_id: str) -> str:
         with self.action_results_store_lock:
@@ -215,8 +253,13 @@ class CancelROS2ActionTool(BaseROS2Tool):
     description: str = "Cancel a ROS2 action"
     args_schema: Type[CancelROS2ActionToolInput] = CancelROS2ActionToolInput
 
+    internal_action_id_mapping: Dict[str, str] = Field(
+        default_factory=get_internal_action_id_mapping
+    )
+
     def _run(self, action_id: str) -> str:
-        self.connector.terminate_action(action_id)
+        external_action_id = self.internal_action_id_mapping[action_id]
+        self.connector.terminate_action(external_action_id)
         return f"Action {action_id} cancelled"
 
 
@@ -228,7 +271,9 @@ class GetROS2ActionIDsTool(BaseTool):
     name: str = "get_ros2_action_ids"
     description: str = "Get the IDs of all ROS2 actions"
     args_schema: Type[GetROS2ActionIDsToolInput] = GetROS2ActionIDsToolInput
-    internal_action_id_mapping: Dict[str, str] = {}
+    internal_action_id_mapping: Dict[str, str] = Field(
+        default_factory=get_internal_action_id_mapping
+    )
 
     def _run(self) -> str:
         return str(list(self.internal_action_id_mapping.keys()))
