c8352155724f9d13f2f714962508b721e905ed65
Kajetan Rachwał <kajetan.rachwal@robotec.ai>
2025-04-25 15:44:51 +0200

    feat: add load config, plus additional typing

--- src/rai_asr/rai_asr/agents/initialization.py
+++ src/rai_asr/rai_asr/agents/initialization.py
@@ -13,24 +13,29 @@
 # limitations under the License.
 
 from dataclasses import dataclass
+from typing import Literal, Optional
+
+import tomli
 
 
 @dataclass
 class VADConfig:
-    model_name: str = "SileroVAD"
+    model_name: Literal["SileroVAD"] = "SileroVAD"
     threshold: float = 0.5
     sampling_rate: int = 1600
 
 
 @dataclass
 class WWConfig:
-    model_name: str = "OpenWakeWord"
+    model_name: Literal["OpenWakeWord"] = "OpenWakeWord"
     threshold: float = 0.01
 
 
 @dataclass
 class TranscribeConfig:
-    model_name: str = "LocalWhisper"
+    model_name: Literal["LocalWhisper", "FasterWhisper", "OpenAIWhisper"] = (
+        "LocalWhisper"
+    )
 
 
 @dataclass
@@ -44,3 +49,29 @@ class ASRAgentConfig:
     wakeword: WWConfig
     transcribe: TranscribeConfig
     microphone: MicrophoneConfig
+
+
+def load_config(config_path: Optional[str] = None) -> ASRAgentConfig:
+    if config_path is None:
+        with open("config.toml", "rb") as f:
+            config_dict = tomli.load(f)
+    else:
+        with open(config_path, "rb") as f:
+            config_dict = tomli.load(f)
+    return ASRAgentConfig(
+        voice_activity_detection=VADConfig(
+            model_name=config_dict["voice_activity_detection"]["model_name"],
+            threshold=config_dict["voice_activity_detection"]["threshold"],
+            sampling_rate=config_dict["voice_activity_detection"]["sampling_rate"],
+        ),
+        wakeword=WWConfig(
+            model_name=config_dict["wakeword"]["model_name"],
+            threshold=config_dict["wakeword"]["threshold"],
+        ),
+        transcribe=TranscribeConfig(
+            model_name=config_dict["transcribe"]["model_name"],
+        ),
+        microphone=MicrophoneConfig(
+            device_name=config_dict["microphone"]["device_name"],
+        ),
+    )
