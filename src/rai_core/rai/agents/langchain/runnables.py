1a67f592717f88d1fd09d20f8913e46e72a764af
Maciej Majek <maciej.majek@robotec.ai>
2025-04-29 22:46:36 +0200

    chore: pre-commit

--- src/rai_core/rai/agents/langchain/runnables.py
+++ src/rai_core/rai/agents/langchain/runnables.py
@@ -31,10 +31,9 @@ from langchain_core.tools import BaseTool
 from langgraph.graph import START, StateGraph
 from langgraph.prebuilt.tool_node import tools_condition
 
-from rai.agents.tool_runner import ToolRunner
-from rai.initialization import get_llm_model
-from rai.messages import HumanMultimodalMessage
 from rai.agents import ToolRunner
+from rai.initialization import get_llm_model
+from rai.messages import HumanMultimodalMessage, SystemMultimodalMessage
 
 
 class ReActAgentState(TypedDict):
