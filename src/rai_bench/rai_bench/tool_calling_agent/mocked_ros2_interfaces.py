ec4f927462214b229ebaacdc29a48c87d060321f
jmatejcz <jakub.matejczyk@robotec.ai>
2025-06-06 17:16:15 +0200

    feat: add service validator

--- src/rai_bench/rai_bench/tool_calling_agent/mocked_ros2_interfaces.py
+++ src/rai_bench/rai_bench/tool_calling_agent/mocked_ros2_interfaces.py
@@ -53,6 +53,23 @@ from rai_bench.tool_calling_agent.messages.topics import AudioMessage, HRIMessag
 # the dict contains custom as well as couple other common interfaces
 
 COMMON_INTERFACES: Dict[str, str] = {
+    "rcl_interfaces/srv/SetParameters": """# A list of parameters to set.
+Parameter[] parameters
+	string name
+	ParameterValue value
+		uint8 type
+		bool bool_value
+		int64 integer_value
+		float64 double_value
+		string string_value
+		byte[] byte_array_value
+		bool[] bool_array_value
+		int64[] integer_array_value
+		float64[] double_array_value
+		string[] string_array_value
+
+---
+# Indicates whether setting each parameter succeeded or not and why.""",
     "sensor_msgs/msg/CameraInfo": """
 # This message defines meta information for a camera. It should be in a
 # camera namespace on topic "camera_info" and accompanied by up to five
