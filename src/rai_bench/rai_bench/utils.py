0dbfad6f5a3c2716ed74ca77dfa5cb5c979e53ce
jmatejcz <jakub.matejczyk@robotec.ai>
2025-08-11 18:07:30 +0200

    refactor: level arg to logger

--- src/rai_bench/rai_bench/utils.py
+++ src/rai_bench/rai_bench/utils.py
@@ -122,12 +122,12 @@ def parse_manipulation_o3de_benchmark_args():
     return parser.parse_args()
 
 
-def define_benchmark_logger(out_dir: Path) -> logging.Logger:
+def define_benchmark_logger(out_dir: Path, level: int = logging.INFO) -> logging.Logger:
     log_file = out_dir / "benchmark.log"
     out_dir.mkdir(parents=True, exist_ok=True)
 
     file_handler = logging.FileHandler(log_file)
-    file_handler.setLevel(logging.INFO)
+    file_handler.setLevel(level)
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
@@ -136,7 +136,7 @@ def define_benchmark_logger(out_dir: Path) -> logging.Logger:
     bench_logger = logging.getLogger("Benchmark logger")
     for handler in bench_logger.handlers:
         bench_logger.removeHandler(handler)
-    bench_logger.setLevel(logging.INFO)
+    bench_logger.setLevel(level)
     bench_logger.addHandler(file_handler)
 
     return bench_logger
