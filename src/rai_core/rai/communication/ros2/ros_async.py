d167803edcad759de559ba0498364a2bcf996c75
maciejmajek <maciej.majek@robotec.ai>
2025-11-15 12:07:53 +0100

    refactor: replace time.sleep with threading.Event for improved future handling in ROS 2 communication

--- src/rai_core/rai/communication/ros2/ros_async.py
+++ src/rai_core/rai/communication/ros2/ros_async.py
@@ -12,26 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import time
-from typing import Any, Optional
+import threading
+from typing import Any
 
+import rclpy
 import rclpy.task
 
 
 def get_future_result(
     future: rclpy.task.Future, timeout_sec: float = 5.0
-) -> Optional[Any]:
+) -> Any | None:
     """Replaces rclpy.spin_until_future_complete"""
     result = None
+    event = threading.Event()
 
     def callback(future: rclpy.task.Future) -> None:
         nonlocal result
         result = future.result()
+        event.set()
 
     future.add_done_callback(callback)
 
-    ts = time.perf_counter()
-    while result is None and time.perf_counter() - ts < timeout_sec:
-        time.sleep(0.1)
+    _ = event.wait(timeout=timeout_sec)
 
     return result
